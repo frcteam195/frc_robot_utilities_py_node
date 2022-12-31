5951d168cb348c8331c93f3c61ed98961e70b433
Robert Hilton <robert.a.hilton.jr@gmail.com>
2022-12-31 04:18:04 +0000

    Update

--- src/frc_robot_utilities_py_node/SubsystemController.py
+++ src/frc_robot_utilities_py_node/SubsystemController.py
@@ -8,14 +8,14 @@ StatusClass = TypeVar('StatusClass')
 
 class SubsystemController(Generic[ControlClass, StatusClass]):
 
-    def __init__(self, control_topic : str, status_topic : str):
-        self.__publisher = rospy.Publisher(name=control_topic, data_class=ControlClass, queue_size=50, tcp_nodelay=True)
-        self.__status_handler = BufferedROSMsgHandlerPy(StatusClass)
+    def __init__(self, control_topic : str, control_data_class, status_topic : str, status_data_class):
+        self.__publisher = rospy.Publisher(name=control_topic, data_class=control_data_class, queue_size=50, tcp_nodelay=True)
+        self.__status_handler = BufferedROSMsgHandlerPy(status_data_class)
         self.__status_handler.register_for_updates(status_topic)
 
     def get(self) -> StatusClass:
         return self.__status_handler.get()
 
     def publish(self, msg : ControlClass):
-        if isinstance(msg, ControlClass):
+        if isinstance(msg, ControlClass.__class__):
             self.__publisher.publish(msg)
