c8b9a7c3720b77ce13c405ea22d30264ab40c881
Michael Todd <eltodd@gmail.com>
2023-01-21 18:19:47 +0000

    Switch to using ck_ros_base_msgs

--- src/frc_robot_utilities_py_node/frc_robot_utilities_py.py
+++ src/frc_robot_utilities_py_node/frc_robot_utilities_py.py
@@ -2,7 +2,7 @@
 
 import tf2_ros
 import rospy
-from rio_control_node.msg import Motor_Status, Robot_Status
+from ck_ros_base_msgs_node.msg import Motor_Status, Robot_Status
 from ck_ros_msgs_node.msg import HMI_Signals
 from frc_robot_utilities_py_node.BufferedROSMsgHandlerPy import BufferedROSMsgHandlerPy
 from frc_robot_utilities_py_node.RobotStatusHelperPy import RobotStatusHelperPy, Alliance, RobotMode
@@ -17,6 +17,6 @@ def register_for_robot_updates():
     global robot_status
     global robot_updates_internal
     global motor_updates_internal
-    
+
     hmi_updates.register_for_updates("/HMISignals")
     robot_updates_internal.register_for_updates("/RobotStatus")
