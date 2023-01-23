b55e2077d93252b250e859bcae135adbc5ccb0c3
Robert Hilton <robert.a.hilton.jr@gmail.com>
2023-01-23 02:06:04 +0000

    Update

--- src/frc_robot_utilities_py_node/frc_robot_utilities_py.py
+++ src/frc_robot_utilities_py_node/frc_robot_utilities_py.py
@@ -2,7 +2,7 @@
 
 import tf2_ros
 import rospy
-from ck_ros_base_msgs_node.msg import Motor_Status, Robot_Status
+from ck_ros_base_msgs_node.msg import Robot_Status
 from ck_ros_msgs_node.msg import HMI_Signals
 from frc_robot_utilities_py_node.BufferedROSMsgHandlerPy import BufferedROSMsgHandlerPy
 from frc_robot_utilities_py_node.RobotStatusHelperPy import RobotStatusHelperPy, Alliance, RobotMode
@@ -16,7 +16,6 @@ def register_for_robot_updates():
     global hmi_updates
     global robot_status
     global robot_updates_internal
-    global motor_updates_internal
 
     hmi_updates.register_for_updates("/HMISignals")
     robot_updates_internal.register_for_updates("/RobotStatus")
