eed51e673a0916bf6623fe4329d8fea56aa0748a
BonomiC <bonomi.c11@gmail.com>
2023-02-15 00:07:23 +0000

    Use set_pose service

--- src/frc_robot_utilities_py_node/frc_robot_utilities_py.py
+++ src/frc_robot_utilities_py_node/frc_robot_utilities_py.py
@@ -4,10 +4,12 @@ import tf2_ros
 from tf.transformations import *
 import rospy
 import nav_msgs.msg
+import geometry_msgs.msg
 from ck_ros_base_msgs_node.msg import Robot_Status
 from ck_ros_msgs_node.msg import HMI_Signals
 from frc_robot_utilities_py_node.BufferedROSMsgHandlerPy import BufferedROSMsgHandlerPy
 from frc_robot_utilities_py_node.RobotStatusHelperPy import RobotStatusHelperPy, Alliance, RobotMode
+from robot_localization.srv import SetPose
 
 from ck_utilities_py_node import geometry, ckmath
 
@@ -17,6 +19,7 @@ robot_updates_internal = BufferedROSMsgHandlerPy(Robot_Status)
 robot_status = RobotStatusHelperPy(robot_updates_internal)
 
 odom_publisher = rospy.Publisher(name="/ResetHeading", data_class=nav_msgs.msg.Odometry, queue_size=10, tcp_nodelay=True)
+set_pose = rospy.ServiceProxy('/set_pose', SetPose)
 
 
 def register_for_robot_updates():
@@ -28,7 +31,7 @@ def register_for_robot_updates():
     robot_updates_internal.register_for_updates("/RobotStatus")
 
 
-def reset_robot_pose(x_inches=0, y_inches=0, heading_degrees=0):
+def reset_robot_pose_old(x_inches=0, y_inches=0, heading_degrees=0):
     global odom_publisher
 
     odom = nav_msgs.msg.Odometry()
@@ -61,3 +64,30 @@ def reset_robot_pose(x_inches=0, y_inches=0, heading_degrees=0):
     odom.twist.covariance = geometry.Covariance().to_msg()
 
     odom_publisher.publish(odom)
+
+def reset_robot_pose(x_inches=0, y_inches=0, heading_degrees=0):
+    global set_pose
+
+    odom = geometry_msgs.msg.PoseWithCovarianceStamped()
+
+    odom.header.stamp = rospy.Time.now()
+    odom.header.frame_id = 'odom'
+    
+    pose = geometry.Pose()
+    pose.position.x = ckmath.inches_to_meters(x_inches)
+    pose.position.y = ckmath.inches_to_meters(y_inches)
+    pose.orientation.yaw = math.radians(heading_degrees)
+
+    odom.pose.pose = pose.to_msg()
+
+    pose_covariance = geometry.Covariance()
+    # pose_covariance.x_var = 0.001
+    # pose_covariance.y_var = 0.001
+    # pose_covariance.z_var = 0.001
+    # pose_covariance.yaw_var = 0.001
+    # pose_covariance.pitch_var = 0.001
+    # pose_covariance.roll_var = 0.001
+
+    odom.pose.covariance = pose_covariance.to_msg()
+
+    set_pose(odom)
